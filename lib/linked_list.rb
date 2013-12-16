b608a5a3639ab588bfa043dd18cdcaf76b5925c2
Lars Söderkvist <updates11@mac.com>
2013-12-16 13:50:47 -0600

    Simplify conditionals and streamline naming.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,55 +8,55 @@ class LinkedList
   end
 
   def add_item(payload)
-    if @head == nil
+    if @head.nil?
       @head = LinkedListItem.new(payload)
     else
-      current_node = @head
-      while current_node.last? == false
-        current_node = current_node.next_list_item
+      current_item = @head
+      until current_item.last?
+        current_item = current_item.next_list_item
       end
-      current_node.next_list_item = LinkedListItem.new(payload)
+      current_item.next_list_item = LinkedListItem.new(payload)
     end
   end
 
   def get(index)
-    current_node = @head
+    current_item = @head
     index.times do
-      raise IndexError, 'Item at index number does not exist' if current_node.nil?
-      current_node = current_node.next_list_item
+      raise IndexError, 'Item at index number does not exist' if current_item.nil?
+      current_item = current_item.next_list_item
     end
     raise IndexError, 'Index number cannot be negative' if index < 0
-    current_node.payload
+    current_item.payload
   end
 
   def size
     count = 0
-    current_node = @head
-    until current_node == nil do
-      current_node = current_node.next_list_item
+    current_item = @head
+    until current_item.nil? do
+      current_item = current_item.next_list_item
       count += 1
     end
     count
   end
 
   def last
-    return @head if @head == nil
-    current_node = @head
-    until current_node.last? do
-      current_node = current_node.next_list_item
+    return @head if @head.nil?
+    current_item = @head
+    until current_item.last? do
+      current_item = current_item.next_list_item
     end
-    current_node.payload
+    current_item.payload
   end
 
   def to_s
-    if @head == nil
+    if @head.nil?
       string = ""
-    else @head.next_list_item == nil
+    else
       string = "#{@head.payload}"
-      current_node = @head.next_list_item
-      while current_node do
-        string << ", #{current_node.payload}"
-        current_node = current_node.next_list_item
+      current_item = @head.next_list_item
+      while current_item do
+        string << ", #{current_item.payload}"
+        current_item = current_item.next_list_item
       end
       string << " "
     end
